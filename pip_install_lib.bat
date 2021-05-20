a8103955781d9a816a55a5455aeb43cebb75aa25
acemark1 <jeyachandranajinath@gmail.com>
2021-05-20 17:00:45 +0100

    Batch file

---
+++ pip_install_lib.bat
@@ -0,0 +1,15 @@
+echo off 
+echo ------------Install Python Libraries------------ 
+echo -----Update/Upgrade pip 
+python.exe -m pip install --upgrade p
+echo -----Install Libraries
+for /f %%a in (lib.txt) do (
+    pip3 install %%a
+)
+
+
+echo -----Checking installation
+python s.py
+
+
+pause
